f3bf5a4e549fd5a68d62ce5101b8963c560a3530
macabdul9 <abdulwaheed1513@gmail.com>
2020-02-16 15:12:26 +0530

    add files via terminal

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button'; 
+import classes from './CheckoutSummary.module.css';   
+
+const CheckoutSummary = props => {
+    return (
+        <div className={classes.CheckoutSummary}>
+            <h1>Hope, it tastes well !</h1>
+            <div style={{width:'100%', margin:'auto'}}>
+                <Burger ingredients={props.ingredients}/>
+            </div>
+            <Button btnType='Danger' clicked>CANCEL</Button>
+            <Button btnType='Success' clicked>CONTINUE</Button>
+        </div>
+    )
+}
+
+
+export default CheckoutSummary;
