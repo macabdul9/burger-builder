73575dfa257added5ffe124a4a6982193fe58469
macabdul9 <abdulwaheed1513@gmail.com>
2020-03-02 23:52:41 +0530

    add files via terminal

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -65,7 +65,7 @@ export const fetchOrders = (token, userId) => {
     return dispatch => {
         dispatch(fetchOrdersStart());
         // const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
-        axios.get( '/orders.json?auth=' + token)
+        axios.get( '/orders.json?auth=', token)
             .then( res => {
                 const fetchedOrders = [];
                 for ( let key in res.data ) {
@@ -81,4 +81,5 @@ export const fetchOrders = (token, userId) => {
                 dispatch(fetchOrdersFail(err));
             } );
     };
+    
 };
